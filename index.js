70c6cb8b2f2fb10992f3ba49ca64410dbd0582be
Aaron Sanchez <saarong12@gmail.com>
2022-12-18 16:17:28 -0600

    Projects page filled out

--- index.js
+++ index.js
@@ -2,13 +2,22 @@ console.log('hello')
 
 const projects = [
   {
-    name: 'Sines Ear Trainers'
+    name: 'Sines Ear Trainer',
+    description:
+      'An ear training game geared towards assisting audio engineers and music students in training their most important assets: their ears.',
+    link: 'https://github.com/Aaron-G-Sanchez/SINES-EAR-TRAINER'
   },
   {
-    name: 'BEERBOOK'
+    name: 'BEERBOOK',
+    description:
+      'A full-stack PERN application that allows users to track and record their favorite brews.',
+    link: 'https://github.com/Aaron-G-Sanchez/BeerBook-Frontend'
   },
   {
-    name: 'Hogwarts Admin System'
+    name: 'Hogwarts Admin System',
+    description:
+      'A full-stack PERN application aimed towards helping the great Professor Albus Dumbledore in keeping track of his great many students.',
+    link: 'https://github.com/EddieYuabov/Hogwarts_frontend'
   }
 ]
 
@@ -17,7 +26,15 @@ let projectsDisplay = document.querySelector('.projects-wrapper')
 projects.forEach((project) => {
   let div = document.createElement('div')
   let heading = document.createElement('h2')
+  let descriptionContainer = document.createElement('div')
+  let description = document.createElement('p')
+
   projectsDisplay.appendChild(div)
+  div.className = 'project-container'
   div.appendChild(heading)
+  div.appendChild(descriptionContainer)
+  descriptionContainer.appendChild(description)
+  descriptionContainer.className = 'project-description'
   heading.innerHTML = `${project.name}`
+  description.innerHTML = `${project.description}`
 })
