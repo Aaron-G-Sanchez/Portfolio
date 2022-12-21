7bf8494357fe44b4db42a8ec6d7ae3bc505ef9b9
Aaron Sanchez <saarong12@gmail.com>
2022-12-21 10:44:45 -0600

    Added resume path

--- about.js
+++ about.js
@@ -2,5 +2,7 @@ let resumeButton = document.querySelector('.show-resume')
 
 resumeButton.addEventListener('click', (e) => {
   e.preventDefault()
-  console.log('resume button clicked')
+  window.open(
+    'https://docs.google.com/document/d/1jG9x4AWfc-yWmlaXVUjVm3s2txJSO6YqmVe9m-rYV4c/edit?usp=sharing'
+  )
 })
