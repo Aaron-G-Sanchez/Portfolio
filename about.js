9d4bc201ec3828573255242f10fed765b2144f9f
Aaron Sanchez <saarong12@gmail.com>
2023-01-03 15:20:22 -0600

    Fixed resume link

--- about.js
+++ about.js
@@ -3,6 +3,6 @@ let resumeButton = document.querySelector('.show-resume')
 resumeButton.addEventListener('click', (e) => {
   e.preventDefault()
   window.open(
-    'https://docs.google.com/document/d/1jG9x4AWfc-yWmlaXVUjVm3s2txJSO6YqmVe9m-rYV4c/edit?usp=sharing'
+    'https://drive.google.com/file/d/1J72gbVOpt391rjX698ZM1F6UxDJHR2xA/view?usp=sharing'
   )
 })
